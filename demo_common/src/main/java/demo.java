01d7bd4c10284426331960e6b9d2ba04a8ea1679
CrazyCat <1798382973@qq.com>
2018-07-14 22:20:12 +0800

    version 1.0

--- demo_common/src/main/java/demo.java
+++ demo_common/src/main/java/demo.java
@@ -2,5 +2,6 @@ public class demo {
     public static void main(String[] args) {
         System.out.println("123");
         System.out.println("123");
+        System.out.println("这是什么");
     }
 }
